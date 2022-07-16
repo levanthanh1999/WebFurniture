aeae91d035a617f98450f90416a0a651af0d1f65
ThanhLe <levanthanh23061999@gmail.com>
2022-07-16 15:54:13 +0700

    funiture

--- website_ecomerce/product.js
+++ website_ecomerce/product.js
@@ -1,13 +1,13 @@
-let starContainerInput = [...document.querySelectorAll('.star-container')]
+// let starContainerInput = [...document.querySelectorAll('.star-container')]
 
-starContainerInput.map((star, index) => {
-    star.addEventListener('click', () => {
-        for (let i = 0; i < 5; i++) {
-            if (i <= index) {
-                starContainerInput[i].src = `img/img/fill star.png`;
-            } else {
-                starContainerInput[i].src = `img/img/no fill star.png`;
-            }
-        }
-    })
-})
+// starContainerInput.map((star, index) => {
+//     star.addEventListener('click', () => {
+//         for (let i = 0; i < 5; i++) {
+//             if (i <= index) {
+//                 starContainerInput[i].src = `img/img/fill star.png`;
+//             } else {
+//                 starContainerInput[i].src = `img/img/no fill star.png`;
+//             }
+//         }
+//     })
+// })
